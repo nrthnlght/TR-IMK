2defe3e9832d842367ee413360aa2e004e8cba22
fdlrahen <108210712+fdlrahen@users.noreply.github.com>
2024-02-21 17:43:55 +0000

    codespaces:supreme capybara

--- gawai.js
+++ gawai.js
@@ -1,18 +1,7 @@
-const exampleModal = document.getElementById('exampleModal')
-if (exampleModal) {
-  exampleModal.addEventListener('show.bs.modal', event => {
-    // Button that triggered the modal
-    const button = event.relatedTarget
-    // Extract info from data-bs-* attributes
-    const recipient = button.getAttribute('data-bs-whatever')
-    // If necessary, you could initiate an Ajax request here
-    // and then do the updating in a callback.
+document.getElementById('open-popup').addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'flex';
+});
 
-    // Update the modal's content.
-    const modalTitle = exampleModal.querySelector('.modal-title')
-    const modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-    modalTitle.textContent = `New message to ${recipient}`
-    modalBodyInput.value = recipient
-  })                                                                                                                                                                                        
-}
+document.getElementsByClassName('close-btn')[0].addEventListener('click', function() {
+  document.getElementById('popup').style.display = 'none';
+});
